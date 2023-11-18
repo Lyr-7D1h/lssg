f8e5f2f079313c86aab17ee513c85d1be2105db7
Lyr <lyr-7d1h@pm.me>
2023-11-18 23:26:27 +0100

    fixed page discovery

--- lib/lssg-lib/src/sitetree/site_tree.rs
+++ lib/lssg-lib/src/sitetree/site_tree.rs
@@ -234,7 +234,7 @@ impl SiteTree {
         if SiteNodeKind::is_stylesheet(&input) {
             return self.add_stylesheet(input, parent_id);
         }
-        if SiteNodeKind::is_stylesheet(&input) {
+        if SiteNodeKind::is_page(&input) {
             return self.add_page(input, parent_id);
         }
 
